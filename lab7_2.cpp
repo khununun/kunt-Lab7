03b59070bd0e4f84a0e902ee27dd2bc7c2563d85
khununun <68842142+khununun@users.noreply.github.com>
2022-12-13 16:30:48 +0700

    Done

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -2,13 +2,33 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 	
 	int i = 0, P[5000], k, result;
-	while(i < 5000){
-		P[i] = 2*i+1;
-		i++;
+	cout << "Please input k: ";
+	cin >> k;
+	if (k > 0)
+	{
+	    result = 0;
+	    while(i < 5000)
+	    {
+    		P[i] = 2*i+1;
+    		if(P[i]%k == 0)
+    		{
+    		    result -= P[i];
+    		}
+    		else
+    		{
+    		    result += P[i];
+    		}
+    		i++;
+	    }
+	cout << "Result = " << result << "\n";
 	}
-	
+	else
+	{
+	    cout << "Invalid input!!!";
+	}
 	return 0;
 }
